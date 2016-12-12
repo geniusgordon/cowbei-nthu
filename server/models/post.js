a6fb401981810630f4f823195091b36ec82c3ed4
Gordon <geniusgordon@gmail.com>
2016-12-12 17:43:04 +0800

    add cowbeiId in post schema

--- server/models/post.js
+++ server/models/post.js
@@ -1,6 +1,7 @@
 export default (sequelize, DataTypes) =>
   sequelize.define('post', {
     id: { type: DataTypes.STRING(40), primaryKey: true },
+    cowbeiId: DataTypes.INTEGER,
     message: DataTypes.TEXT,
     created_time: DataTypes.DATE,
   }, {
