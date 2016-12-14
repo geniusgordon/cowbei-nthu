54039e836459d75e49e6ece161d2065bb56b2c3e
Gordon <geniusgordon@gmail.com>
2016-12-14 22:08:03 +0800

    [server] static file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import expressValidator from 'express-validator';
+import path from 'path';
 import router from './routes';
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(expressValidator());
 app.use(router);
 app.use((req, res) => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import path from 'path';
 import posts from './posts';
 
 const router = new Router();
+router.use('/p', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
+});
 router.use('/api/posts', posts);
 
 export default router;
