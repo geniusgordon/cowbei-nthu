ac16cc45e54f2e69ba6926ff83bb870606c11944
Gordon <geniusgordon@gmail.com>
2016-12-14 20:36:33 +0800

    parse submitted_time

--- server/models/post.js
+++ server/models/post.js
@@ -1,9 +1,10 @@
 export default (sequelize, DataTypes) =>
   sequelize.define('post', {
     id: { type: DataTypes.STRING(40), primaryKey: true },
-    cowbeiId: DataTypes.INTEGER,
+    cowbei_id: DataTypes.INTEGER,
     message: DataTypes.TEXT,
     created_time: DataTypes.DATE,
+    submitted_time: DataTypes.DATE,
   }, {
     timestamps: false,
   });
