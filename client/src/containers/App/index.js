fccaa8c99ca681abf00d7e3bf056ff1960535274
Gordon <geniusgordon@gmail.com>
2016-12-15 15:03:42 +0800

    [client] fixed app bar

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -4,26 +4,37 @@ import AppBar from 'material-ui/AppBar';
 import Router from 'react-router/BrowserRouter'
 import Match from 'react-router/Match'
 import Redirect from 'react-router/Redirect'
+import styled from 'styled-components';
 import PostList from '../PostList';
 
 const RedirectP = () => <Redirect to="/p" />;
+const MainWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+const StyledAppBar = styled(AppBar)`
+  position: fixed !important;
+`;
+const BodyWrapper = styled.div`
+  padding-top: 64px;
+`;
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div>
-          <AppBar
-            title="Title"
+        <MainWrapper>
+          <StyledAppBar
+            title="靠北清大"
             iconClassNameRight="muidocs-icon-navigation-expand-more"
           />
           <Router>
-            <div>
+            <BodyWrapper>
               <Match exactly pattern="/" component={RedirectP} />
               <Match pattern="/p" component={PostList} />
-            </div>
+            </BodyWrapper>
           </Router>
-        </div>
+        </MainWrapper>
       </MuiThemeProvider>
     );
   }
